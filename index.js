0f527a2599b19cc486b7ef0a7cd78ec4579b1553
Eric Kim <hyeokjungkim@gmail.com>
2018-04-26 10:02:12 -0400

    Done.

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  let list = document.querySelectorAll('.ranked-list')
+  let list = document.querySelectorAll('.ranked-list li')
   for (let i = 0; i < list.length; i++) {
-    let children = list[i].children
-
-    for (let j = 0; j < children.length; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n
-    }
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
   }
 }
 
